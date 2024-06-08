258d73deb9db73e1d1a436908fda5d597955b403
sweoo <swezinoo410@gmail.com>
2024-06-08 14:54:36 -0600

    add studentReducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import courseReducer from '../features/courses/courseSlice'; 
+import studentReducer from '../features/students/studentslice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     course: courseReducer,
+    students:studentReducer,
   },
 });
